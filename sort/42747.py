1d7b0636a48d5e759f255ac604cb138e23f2ea31
이민호 <lkjjr0424@hanmail.net>
2021-03-14 14:25:13 +0900

    H-Index

---
+++ sort/42747.py
@@ -0,0 +1,10 @@
+def solution(citations):
+    answer = 0
+    
+    n = len(citations)
+    citations.sort(reverse=True)
+    
+    while answer < n and citations[answer] >= answer+1:
+        answer += 1
+    
+    return answer
